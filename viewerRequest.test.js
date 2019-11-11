cc66dae713d109bfac4a76be8d5c24f3aff9d105
堀家隆宏 <horiketakahiro@horiketakahironoMacBook-Pro.local>
2019-11-12 08:06:02 +0900

    fix lint

--- viewerRequest.test.js
+++ viewerRequest.test.js
@@ -3,13 +3,52 @@ const expect = require('chai').expect
 const { handler } = require('./viewerRequest')
 
 describe('#viewerRequest()', () => {
-    it('should set google flag if client ip is range of google', () => {
+  it('should set google flag if client ip is range of google', async () => {
+    const event = {
+      Records: [
+        {
+          cf: {
+            request: {
+              clientIp: '66.249.90.77',
+              headers: {
+                accept: [
+                  {
+                    key: 'accept',
+                    value: 'image/webp,image/apng,image/*,*/*;q=0.8'
+                  }
+                ]
+              }
+            }
+          }
+        }
+      ]
+    }
+    handler(event, {}, (error, request) => {
+      expect(request).to.deep.equal({
+        headers: {
+          accept: [
+            {
+              key: 'accept',
+              value: 'image/webp,image/apng,image/*,*/*;q=0.8'
+            }
+          ],
+          'is-google-bot-request': [
+            {
+              key: 'Is-Google-Bot-Request',
+              value: '0'
+            }
+          ]
+        }
+      })
+    })
+  }),
+    it('should not set google flag if client ip is not range of google', () => {
       const event = {
         Records: [
           {
             cf: {
               request: {
-                clientIp: '66.249.90.77',
+                clientIp: 'xx.xx.xx.xx',
                 headers: {
                   accept: [
                     {
@@ -41,45 +80,5 @@ describe('#viewerRequest()', () => {
           }
         })
       })
-    }),
-
-    it('should not set google flag if client ip is not range of google', () => {
-        const event = {
-          Records: [
-            {
-              cf: {
-                request: {
-                  clientIp: 'xx.xx.xx.xx',
-                  headers: {
-                    accept: [
-                      {
-                        key: 'accept',
-                        value: 'image/webp,image/apng,image/*,*/*;q=0.8'
-                      }
-                    ]
-                  }
-                }
-              }
-            }
-          ]
-        }
-        handler(event, {}, (error, request) => {
-          expect(request).to.deep.equal({
-            headers: {
-              accept: [
-                {
-                  key: 'accept',
-                  value: 'image/webp,image/apng,image/*,*/*;q=0.8'
-                }
-              ],
-              'is-google-bot-request': [
-                {
-                  key: 'Is-Google-Bot-Request',
-                  value: '0'
-                }
-              ]
-            }
-          })
-        })
-      })
+    })
 })
